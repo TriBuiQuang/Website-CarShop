67d5842b6b1d95ed7f5cdb85e19cf4add5ccfcb1
Bùi Quang Đức Tri <buiquangductri@gmail.com>
2019-03-18 22:29:52 +0700

    Initization web car

---
+++ Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Car.Models;
+
+namespace Car
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            
+            Application["VisitorsCount"] = 0;
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            
+            Application.Lock();
+            Session["VisitorsCount"] = (int)Application["VisitorsCount"] + 1;
+            Application.UnLock();
+        }
+    }
+}
