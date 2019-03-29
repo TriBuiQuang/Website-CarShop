816444c57345a2a1efbfe5f9fb8016131ca760a4
banhmi123 <37664809+banhmi123@users.noreply.github.com>
2019-03-29 22:20:03 +0700

    AdminController and CarController and GioHangController

---
+++ Controllers/CarController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Car.Models;
+
+using PagedList;
+using PagedList.Mvc;
+
+namespace Car.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: Car
+       
+        dbCAR2DataContext data = new dbCAR2DataContext();
+        private List<Xe> Layxemoi(int count)
+        {
+            return data.Xes.OrderByDescending(a => a.NamSanXuat).Take(count).ToList();
+        }
+        private List<SanPham> Layspmoi(int count)
+        {
+            return data.SanPhams.OrderByDescending(b => b.TenSP).Where(b => b.Xoa == false).Take(count).ToList();
+        }
+
+        public ActionResult Index(int ? page,VIEW v)
+        {
+            if (Session["VisitorsCount"] != null)
+            {
+                
+                DateTime today = DateTime.Now.Date;
+                var v1 = data.VIEWs.Where(a => a.Ngay == today).FirstOrDefault();
+                if (v1 == null)
+                {
+                    v.Ngay = today;
+                    v.View1 += 1;
+                    data.VIEWs.InsertOnSubmit(v);
+                    data.SubmitChanges();
+                }
+                else
+                {
+                    VIEW sp = data.VIEWs.SingleOrDefault(n => n.Ngay == today);
+                    sp.View1 += 1;
+                    UpdateModel(sp);
+                    data.SubmitChanges();
+                }
+            }
+            int pageSize = 8;
+            int pageNum = (page ?? 1);
+
+            var xemoi = Layxemoi(10);
+            return View(xemoi.ToPagedList(pageNum,pageSize));
+        }
+        public ActionResult Index2(int ? page)
+        {
+            int pageSize = 8;
+            int pageNum = (page ?? 1);
+
+            var xemoi = Layxemoi(10);
+            return View(xemoi.ToPagedList(pageNum, pageSize));
+        }
+        public ActionResult Index3(int ? page)
+        {
+            int pageSize = 8;
+            int pageNum = (page ?? 1);
+
+            var ptmoi = Layspmoi(10);
+            return View(ptmoi.ToPagedList(pageNum, pageSize));
+        }
+        public ActionResult SPSXGia(int? page)
+        {
+            
+                int pageNumber = (page ?? 1);
+                int pageSize = 7;
+            var ptmoi = Layspmoi(10);
+            return View(ptmoi.OrderBy(n => n.GiaBan).Where(n=>n.GiaBan<50).ToPagedList(pageNumber, pageSize));
+            
+        }
+        public ActionResult Details2(int id)
+        {
+            var sp = from s in data.SanPhams
+                     where s.MaSP == id
+                     select s;
+            return View(sp.Single());
+        }
+        public ActionResult Details1(int id)
+        {
+            var xe = from x in data.Xes
+                     where x.Maxe == id
+                     select x;
+            return View(xe.Single());
+        }
+        public ActionResult Intro()
+        {
+            return View();
+        }
+        public ActionResult Search(string searchTerm)
+        {
+            var xes = from b in data.Xes select b;
+
+            if (!String.IsNullOrEmpty(searchTerm))
+            {
+                xes = data.Xes.Where(b => b.Tenxe.Contains(searchTerm));
+            }
+            ViewBag.SearchTerm = searchTerm;
+            return View(xes);
+        }
+    }
+}
---
+++ Controllers/GiohangController.cs
@@ -0,0 +1,157 @@
+﻿using Car.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Car.Controllers
+{
+    public class GiohangController : Controller
+    {
+        // GET: Giohang
+        dbCAR2DataContext data = new dbCAR2DataContext();
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang == null)
+            {
+                lstGiohang = new List<Giohang>();
+                Session["Giohang"] = lstGiohang;
+            }
+            return lstGiohang;
+        }
+        public ActionResult ThemGiohang(int iMaSP, string strURL)
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.Find(n => n.iMaSP == iMaSP);
+            if (sanpham == null)
+            {
+                sanpham = new Giohang(iMaSP);
+                lstGiohang.Add(sanpham);
+                return RedirectToAction("Index3", "Car");
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return RedirectToAction("Index3", "Car");
+            }
+        }
+        private int TongSoLuong()
+       {
+                int iTongSoLuong = 0;
+                List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang> ;
+                if(lstGiohang!=null)
+                {
+                    iTongSoLuong = lstGiohang.Sum(n => n.iSoluong);
+                }
+            return iTongSoLuong;
+       }
+        private double TongTien()
+        {
+            double iTongtien = 0;
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                iTongtien = lstGiohang.Sum(n => n.dThanhtien);
+            }
+            return iTongtien;
+        }
+        public ActionResult Giohang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            if(lstGiohang.Count==0)
+            {
+                return RedirectToAction("Index", "Car");
+            }
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return View(lstGiohang);
+        }
+        public ActionResult GiohangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return PartialView();
+        }
+        public ActionResult XoaGiohang(int iMaSP)
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.SingleOrDefault(n => n.iMaSP == iMaSP);
+            if(sanpham!=null)
+            {
+                lstGiohang.RemoveAll(n => n.iMaSP == iMaSP);
+                return RedirectToAction("Giohang");
+            }
+            if(lstGiohang.Count==0)
+            {
+                return RedirectToAction("Index3", "Car");
+            }return RedirectToAction("Giohang");
+        }
+        public ActionResult CapnhatGiohang(int iMaSP, FormCollection f)
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            Giohang sanpham = lstGiohang.SingleOrDefault(n => n.iMaSP == iMaSP);
+            if(sanpham!=null)
+            {
+                sanpham.iSoluong = int.Parse(f["txtSoluong"].ToString());
+            }return RedirectToAction("Giohang");
+        }
+        public ActionResult XoaAllGiohang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            lstGiohang.Clear();
+            return RedirectToAction("Index", "Car");
+        }
+        [HttpGet]
+        public ActionResult Dathang()
+        {
+            if (Session["KH"] == null || Session["KH"].ToString() == "")
+            {
+                return RedirectToAction("Dangnhap", "Nguoidung");
+            }
+            if (Session["Giohang"] == null)
+            {
+                return RedirectToAction("Index", "Car");
+            }
+            List<Giohang> lstGiohang = Laygiohang();
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+
+            return View(lstGiohang);
+        }
+        public ActionResult Dathang(FormCollection collection)
+        {
+            DonHang dh = new DonHang();
+            KhachHang kh = (KhachHang)Session["KH"];
+            List<Giohang> gh = Laygiohang();
+            CTDH ctdh = new CTDH();
+            //dh.MaKH = kh.MaKH;
+            dh.MaKH = kh.MaKH;
+            dh.NgayDat = DateTime.Now;
+            dh.TinhTrangGiaoHang = false;
+            data.DonHangs.InsertOnSubmit(dh);
+            data.SubmitChanges();
+            foreach (var item in gh)
+            {
+                
+                ctdh.MaDH = dh.MaDH;
+                ctdh.MaSP = item.iMaSP;
+                ctdh.SoLuong= item.iSoluong;
+                ctdh.Dongia = (decimal)item.dDongia;
+                data.CTDHs.InsertOnSubmit(ctdh);
+            }
+            
+
+            data.SubmitChanges();
+            Session["Giohang"] = null;
+            return RedirectToAction("Xacnhandonhang", "Giohang");
+        }
+        public ActionResult Xacnhandonhang()
+        {
+            return View();
+        }
+    }
+    
+}
